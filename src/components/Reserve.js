32a635d861f38c2f82a615ec47dc113c942b3d43
vmro2002 <amro_official@hotmail.com>
2023-01-18 01:52:59 +0800

    Updated reservations UI

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {HStack, VStack, useMediaQuery, Text, Box, Button, Spacer} from '@chakra-ui/react'
+import React, {useState} from 'react'
+import {HStack, VStack, useMediaQuery, Text, Box, Button, Spacer, Input} from '@chakra-ui/react'
 import {
     NumberInput,
     NumberInputField,
@@ -12,35 +12,33 @@ import {
     MenuButton,
     MenuList,
     MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
   } from '@chakra-ui/react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons"
 
 export default function Reserve(){
     const [isLargerThan800] = useMediaQuery('(min-width: 1000px)')
+    const [placeholder, setPlaceholder] = useState('Occasion')
+
     return(
         <>
         <Text fontFamily='Markazi Text' fontWeight='bold' fontSize='4em' pl={isLargerThan800? '40':'5'} py='4'>Reservations</Text>
         <VStack px={isLargerThan800? '40' : '5'} spacing={5}>
-            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='5'>
+            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='5' minH='60px'>
                <HStack>
                     <Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Date</Text>
                     <Spacer/>
                     <input type='date'/>
                </HStack>
             </Box>
-            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='5'>
+            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='5' minH='60px'>
                <HStack>
                     <Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Time</Text>
                     <Spacer/>
                     <input type='time'/>
                </HStack>
             </Box>
-            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='5'>
+            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='4'>
                <HStack>
                     <Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Number of guests</Text>
                     <Spacer/>
@@ -53,17 +51,20 @@ export default function Reserve(){
                     </NumberInput>
                </HStack>
             </Box>
-            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='5'>
+            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='3'>
                 <Menu>
-                    <MenuButton as={Button} rightIcon={<FontAwesomeIcon icon={faChevronDown} minW='sm'/>}>
-                        <Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Occasion</Text>
+                    <MenuButton as={Button} rightIcon={placeholder === "Occasion"? <FontAwesomeIcon icon={faChevronDown}/>:<FontAwesomeIcon icon={faChevronUp}/>} onClick={() => setPlaceholder("Occasion")} minW='xs' mx='5'>
+                        <Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>{placeholder}</Text>
                     </MenuButton>
-                    <MenuList>
-                        <MenuItem justifyContent='center'><Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Birthday</Text></MenuItem>
-                        <MenuItem justifyContent='center'><Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Anniversary</Text></MenuItem>
-                        <MenuItem justifyContent='center'><Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Engagement</Text></MenuItem>
+                    <MenuList minW='xs'>
+                        <MenuItem justifyContent='center' onClick={() => setPlaceholder("Birthday")}><Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Birthday</Text></MenuItem>
+                        <MenuItem justifyContent='center' onClick={() => setPlaceholder("Anniversary")}><Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Anniversary</Text></MenuItem>
+                        <MenuItem justifyContent='center' onClick={() => setPlaceholder("Engagement")}><Text fontFamily='Karla' fontSize='1em' fontWeight='bold'>Engagement</Text></MenuItem>
                     </MenuList>
                 </Menu>
+            </Box>
+            <Box borderRadius='lg' borderWidth='1px' minW='sm' borderColor='black' p='4'>
+                <Input size='sm' placeholder="Please enter your name"/>
             </Box>
             <Button colorScheme='yellow'>Submit</Button>
         </VStack>
