a50a0d0550c39733f354ba7a08a78092b02e3623
vmro2002 <amro_official@hotmail.com>
2024-05-18 20:45:33 +0800

    added logout button to dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,20 @@
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Dashboard = () => {
+
+  const { logout } = useAuth0(); //logout function ends the session
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
   return (
     <div>
        <h1>Dashboard</h1>
+       <button onClick={handleLogout}>Log Out</button>
     </div>
   )
 }
